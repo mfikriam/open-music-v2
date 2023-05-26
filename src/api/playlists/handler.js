e953002272004dc382aaba508fa7aa195395d254
mfikriam <yapkun17@gmail.com>
2023-05-26 23:00:50 +0800

    add getPlaylistSongsHandler

--- src/api/playlists/handler.js
+++ src/api/playlists/handler.js
@@ -78,6 +78,18 @@ class PlaylistsHandler {
 
     return response;
   }
+
+  async getPlaylistSongsHandler(request) {
+    const { id: credentialId } = request.auth.credentials;
+    const playlists = await this._service.getPlaylists(credentialId);
+
+    return {
+      status: 'success',
+      data: {
+        playlists,
+      },
+    };
+  }
 }
 
 module.exports = PlaylistsHandler;
